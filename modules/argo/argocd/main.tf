bf2306b7f7cfb300198cd4a74308e45c4afc05fd
Adilson Menechini <adilsonmenechini@gmail.com>
2024-02-17 15:47:43 -0300

    feat: update version argocd

--- modules/argo/argocd/main.tf
+++ modules/argo/argocd/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argocd" {
 
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.51.6"
+  version          = "6.1.0"
   
   namespace        = "argocd"
   create_namespace = true
