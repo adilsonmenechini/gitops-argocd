b18dea3235d4bbf9901921d83d4f864a7ed5b9a0
Adilson Menechini <adilsonmenechini@gmail.com>
2024-02-22 19:57:23 -0300

    feat: update output kind

--- kind/output.tf
+++ kind/output.tf
@@ -4,6 +4,8 @@ output "cluster_info" {
       container_ports = {
         for mapping in instance.kind_config[0].node[0].extra_port_mappings : mapping.container_port => mapping.host_port
       }
-    }
+      api_server_address = instance.kind_config[0].networking[0].api_server_address
+      api_server_port = instance.kind_config[0].networking[0].api_server_port
+    } 
   }
 }
